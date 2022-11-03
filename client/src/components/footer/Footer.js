094013f9dd2c5a892e71f5366f5870853e511e2b
kuba7134 <kstanczyk7134@gmail.com>
2022-11-03 18:29:52 +0100

    footer responsiveness

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,9 +1,19 @@
 import "./footer.css";
-import { BsFillTelephoneFill, BsInstagram, BsFacebook } from "react-icons/bs";
+import {
+  BsFillTelephoneFill,
+  BsInstagram,
+  BsFacebook,
+} from "react-icons/bs";
+import { IoIosMail, IoIosCopy } from "react-icons/io";
 import { SiGooglemaps } from "react-icons/si";
 import { forwardRef } from "react";
 
 const Footer = forwardRef((props, ref) => {
+  const copyNumber = () => {
+    navigator.clipboard.writeText("91124017181111000010981843");
+    alert("Skopiowano numer rachunku bankowego.");
+  };
+
   return (
     <div ref={ref} className="footer">
       <div className="footer-basic">
@@ -19,27 +29,77 @@ const Footer = forwardRef((props, ref) => {
       <div className="footer-container">
         <div className="footer-address footer-column">
           <h3>Adres</h3>
-          <a href="https://goo.gl/maps/ichHV2LVnGVfmmpR6" target="blank"><SiGooglemaps /></a>
-          <p>ul. św Filipa Neri 1</p>
+          <a
+            href="https://goo.gl/maps/ichHV2LVnGVfmmpR6"
+            target="blank"
+          >
+            <SiGooglemaps />
+            <p className="text-no-wrap">ul. św Filipa Neri 1</p>
+          </a>
           <p>Tomaszów Mazowiecki</p>
-        </div>
-        <div className="footer-contact footer-column">
-          <h3>Kontakt</h3>
-          <div className="footer-phone-number">
-            <a href="tel:501815508">
-              <BsFillTelephoneFill /></a>
-            <a href="tel:501815508">
-              <p>501 815 508</p>
-            </a>
-          </div>
         </div>
         <div className="footer-media footer-column">
           <h3>Media</h3>
           <div className="footer-media-box">
-            <a href="https://www.instagram.com/przystandwunastu/" target="blank"><BsInstagram /></a>
-            <a href="https://www.facebook.com/FilipiniTomaszow" target="blank"><BsFacebook /></a>
+            <a
+              href="https://www.instagram.com/przystandwunastu/"
+              target="blank"
+            >
+              <BsInstagram />
+            </a>
+            <a
+              href="https://www.facebook.com/FilipiniTomaszow"
+              target="blank"
+            >
+              <BsFacebook />
+            </a>
           </div>
         </div>
+        <div className="footer-contact footer-column">
+          <h3>Kontakt</h3>
+          <a href="tel:501815508">
+            <BsFillTelephoneFill />
+            <p className="text-no-wrap">501 815 508</p>
+          </a>
+          <a
+            className="footer-mail"
+            href="mailto:tomaszow-rodziny@archidiecezja.lodz.pl"
+          >
+            <IoIosMail />
+            <p>tomaszow-rodziny@archidiecezja.lodz.pl</p>
+          </a>
+        </div>
+        <div className="footer-organizacje footer-column">
+          <h3>Powiązane organizacje</h3>
+          <a
+            href="https://www.facebook.com/przedszkolewyspaszkrabow"
+            target="blank"
+          >
+            Przedszkole Wyspa Szkrabów im. św. Filipa Neri
+          </a>
+          <a href="http://hospicjum-tomaszow.pl/" target="blank">
+            Hospicjum Tomaszów Mazowiecki
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=100064390315446"
+            target="blank"
+          >
+            Tomaszowska Pielgrzymka Rowerowa na Jasną Górę
+          </a>
+          <a
+            href="https://www.facebook.com/przystandwunastu"
+            target="blank"
+          >
+            Przystań Dwunastu
+          </a>
+        </div>
+      </div>
+      <div className="footer-konto">
+        <p style={{ fontWeight: 700 }}>Konto:</p>
+        <p>Bank Pekao S.A. w Tomaszowie Maz.</p>
+        <p className="footer-konto-number" onClick={copyNumber}>
+          91 1240 1718 1111 0000 1098 1843 <IoIosCopy />
+        </p>
       </div>
     </div>
   );
--- client/src/containers/parafia/ksieza/Ksieza.js
+++ client/src/containers/parafia/ksieza/Ksieza.js
@@ -1,44 +1,47 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import "./ksieza.css"
-import chirk from "../resources/chirk.jpg"
-import bogusz from "../resources/bogusz.jpg"
-import porczynski from "../resources/porczynski.jpg"
-import stepien from "../resources/stepien.jpg"
-import wojtunik from "../resources/wojtunik.jpg"
+import React from "react";
+import { motion } from "framer-motion";
+import "./ksieza.css";
+import chirk from "../resources/chirk.jpg";
+import bogusz from "../resources/bogusz.jpg";
+import stepien from "../resources/stepien.jpg";
+import wojtunik from "../resources/wojtunik.jpg";
 
 const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1 },
-}
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
 
 const ksieza = () => {
-    return (
-        <motion.div
-            className="ksieza"
-            key="ksieza"
-            variants={containerVariants}
-            initial="hidden"
-            animate="visible"
-            exit="hidden">
-            <div className="ksieza-single">
-                <img src={wojtunik} alt="ks. Wacław Wojtunik" />
-                <h3>ks. Wacław Wojtunik COr</h3>
-            </div>
-            <div className="ksieza-single">
-                <img src={bogusz} alt="ks. Sławomir Bogusz" />
-                <h3>ks. Sławomir Bogusz COr</h3>
-            </div>
-            <div className="ksieza-single">
-                <img src={chirk} alt="ks. Grzegorz Chirk" />
-                <h3>ks. Grzegorz Chirk COr<br /> - Superior i Proboszcz</h3>
-            </div>
-            <div className="ksieza-single">
-                <img src={stepien} alt="ks. Piotr Stępień" />
-                <h3>ks. Piotr Stępień COr</h3>
-            </div>
-        </motion.div>
-    )
-}
+  return (
+    <motion.div
+      className="ksieza"
+      key="ksieza"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+    >
+      <div className="ksieza-single">
+        <img src={wojtunik} alt="ks. Wacław Wojtunik" />
+        <h3>ks. Wacław Wojtunik COr</h3>
+      </div>
+      <div className="ksieza-single">
+        <img src={bogusz} alt="ks. Sławomir Bogusz" />
+        <h3>ks. Sławomir Bogusz COr</h3>
+      </div>
+      <div className="ksieza-single">
+        <img src={chirk} alt="ks. Grzegorz Chirk" />
+        <h3>
+          ks. Grzegorz Chirk COr
+          <br /> - Superior i Proboszcz
+        </h3>
+      </div>
+      <div className="ksieza-single">
+        <img src={stepien} alt="ks. Piotr Stępień" />
+        <h3>ks. Piotr Stępień COr</h3>
+      </div>
+    </motion.div>
+  );
+};
 
-export default ksieza
+export default ksieza;
